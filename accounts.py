dac27a01e549407750721444e7d97e107840c4c2
Ian Bellamy <bellamy.ian.m@gmail.com>
2024-09-22 09:42:15 -0700

    create basic test cases for transaction mapping

--- accounts.py
+++ accounts.py
@@ -1,21 +1,27 @@
+from time import time
 from sqlalchemy.orm import DeclarativeBase
 
 from typing import List
 
 from sqlalchemy import ForeignKey
 from sqlalchemy import Integer
+from sqlalchemy import Float
 from sqlalchemy import String
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column
 from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
 
 ACCOUNT_TYPES = ["ASSET", "LIABILITY", "EQUITY", "REVENUE", "EXPENSE"]
-DEBIT = "L"
-CREDIT = "R"
+DEBIT = "Dr"
+CREDIT = "Cr"
 NORMAL_BALANCES = [DEBIT, CREDIT]
+
 
 class Base(DeclarativeBase):
     pass
+
 
 class Account(Base):
     """
@@ -25,9 +31,16 @@ class Account(Base):
 
     id = mapped_column(Integer, primary_key=True)
     name: Mapped[str]
-    normal_balance = mapped_column(default=DEBIT)
+    normal_balance: Mapped[str] = mapped_column(default=DEBIT)
 
     # schematic relationships
+    actions: Mapped["Action"] = relationship()
+
+    # dunders
+    def __repr__(self) -> str:
+        return "<Account(id={}, name={}, normal_balance={}".format(
+            self.id, self.name, self.normal_balance
+        )
 
 
 class Action(Base):
@@ -37,45 +50,27 @@ class Action(Base):
     __tablename__ = "action"
 
     id = mapped_column(Integer, primary_key=True)
-    description: Mapped[str]
+    description: Mapped[str] = mapped_column(String, nullable=True)
     amount: Mapped[float]
     side: Mapped[str]
     order: Mapped[int]
 
     # schematic relationships
-    t_id = mapped_column(ForeignKey("t.id"))
+    transaction_id = mapped_column(ForeignKey("transaction.id"))
+    transaction: Mapped["Transaction"] = relationship(back_populates="actions")
 
-class Debitor(Base):
-    """
-    Left side of any accounting transaction, itself an action
-    """
-    __tablename__ = "debitor"
+    account_id = mapped_column(ForeignKey("account.id"))
+    account: Mapped["Account"] = relationship(back_populates="actions")
 
-    id = mapped_column(Integer, primary_key=True)
-    name: Mapped[str]
+    # dunders
+    def __repr__(self):
+        return "<Action: (id={}, amount={}, order={}, transaction_id={}, account_id={})".format(
+            self.id, self.amount, self.order, self.transaction_id, self.account_id
+        )
 
-    # relationship schemas
+    def __str__(self):
+        return "{} {} {}".format(self.order, self.side, self.amount)
 
-class Creditor(Base):
-    """
-    Right side of any accounting transaction, itself an action
-    """
-    __tablename__ = "creditor"
-
-    id = mapped_column(Integer, primary_key=True)
-    name: Mapped[str]
-
-    # relationship schemas
-
-class T(Base):
-    """
-    Implements common elements of all T-structs
-    """
-    __tablename__ = "t"
-
-    id = mapped_column(Integer, primary_key=True)
-    debitor_id: Mapped[int] = mapped_column(ForeignKey("debitor.id"))
-    creditor_id: Mapped[str] = mapped_column(ForeignKey("creditor.id"))
 
 class Transaction(Base):
     """
@@ -84,9 +79,46 @@ class Transaction(Base):
     __tablename__ = "transaction"
 
     id = mapped_column(Integer, primary_key=True)
+    time = mapped_column(Float, default=time)
 
     # schematic relationships
     actions: Mapped[List["Action"]] = relationship()
 
+    # dunders
+    def __str__(self):
+        retValue = "-----------------------\n"
+        for action in self.actions:
+            if action.side == DEBIT:
+                retValue += str(action) + " | \n"
+            elif action.side == CREDIT:
+                retValue += "           | " + str(action) + "\n"
+        return retValue
 
 
+if __name__ == "__main__":
+    engine = create_engine("sqlite://", echo=True)
+    Base.metadata.create_all(engine)
+    session = Session(engine)
+
+    transaction = Transaction()
+
+    # initialize some basic accounts
+    debitor = Account(name="My Company Accounts Receivable")
+    creditor = Account(name="Your Company Accounts Payable")
+    session.add_all([debitor, creditor, transaction])
+    session.commit()
+
+    # create basic line items
+    action1 = Action(transaction_id=transaction.id, side=DEBIT, order=1, amount=100.00, account_id=debitor.id)
+    action2 = Action(transaction_id=transaction.id, side=CREDIT, order=2, amount=100.00, account_id=creditor.id)
+    transaction.actions = [action1, action2]
+
+    print(transaction)
+    print(transaction.actions)
+    session.add_all([transaction, action1, action2])
+    session.commit()
+
+    # print a general ledger and transaction
+    print(session.query(Action).filter(Action.side == CREDIT).all())
+    print(session.query(Action).filter(Action.side == DEBIT).all())
+    print(session.query(Transaction).first())
